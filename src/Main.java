244c3969fdf7127403ffc4e5bf9fd42865847ed3
Maria <maria.bunegina@mail.ru>
2022-09-30 23:46:54 +0300

    дз массивы 2 часть

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
                 min = expense;
             }
         }
-        System.out.println("Минимальная сумма трат за день составила" + min + "рублей");
+        System.out.println("Минимальная сумма трат за день составила" + min + "Рублей");
         System.out.println("Максимальная сумма трат за день составила" + max + "Рублей");
         double averageExpence = (double) sum / expenses.length;
         System.out.println(" Средняя сумма трат за месяц составила" + averageExpence);
